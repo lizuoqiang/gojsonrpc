382b35fc1d7bd123ea9664a5d2babd9be4a71682
zuoqiangl <1084259251@qq.com>
2021-12-20 10:04:24 +0800

    fix read

--- client/tcp.go
+++ client/tcp.go
@@ -3,6 +3,7 @@ package client
 import (
 	"fmt"
 	"github.com/lizuoqiang/gojsonrpc/common"
+	"io"
 	"net"
 	"strconv"
 	"time"
@@ -25,7 +26,7 @@ type TcpOptions struct {
 func NewTcpClient(ip string, port string) (*Tcp, error) {
 	options := TcpOptions{
 		"\r\n",
-		1024,
+		1024 * 1024 * 2,
 		15,
 	}
 	var addr = fmt.Sprintf("%s:%s", ip, port)
@@ -103,34 +104,34 @@ func (p *Tcp) handleFunc(b []byte, result interface{}) error {
 	if err != nil {
 		return err
 	}
-	var buf = make([]byte, p.Options.PackageMaxLength)
-	//p.Conn.SetReadDeadline(time.Now().Add(p.Options.ReadTimeOut * time.Second))
-	n, err := p.Conn.Read(buf)
-	fmt.Println("buf:", string(buf), ",n:", n, ",err:", err)
-	if err != nil {
-		return err
-	}
-	l := len([]byte(p.Options.PackageEof))
-	buf = buf[:n-l]
-
 	//var buf = make([]byte, p.Options.PackageMaxLength)
-	//var tmp = make([]byte, 2048)
-	//for {
-	//	_, err := p.Conn.Read(tmp)
-	//	fmt.Println("read: ", string(tmp), ",err:", err)
-	//	if err != nil {
-	//		if err != io.EOF {
-	//			return err
-	//		}
-	//		break
-	//	}
-	//	buf = append(buf, tmp...)
+	//p.Conn.SetReadDeadline(time.Now().Add(p.Options.ReadTimeOut * time.Second))
+	//n, err := p.Conn.Read(buf)
+	//fmt.Println("buf:", string(buf), ",n:", n, ",err:", err)
+	//if err != nil {
+	//	return err
 	//}
-	////todo
-	//fmt.Println("buf1:", string(buf))
-	//eofLen := len([]byte(p.Options.PackageEof))
-	//bufLen := len(buf)
-	//buf = buf[:bufLen-eofLen]
+	//l := len([]byte(p.Options.PackageEof))
+	//buf = buf[:n-l]
+
+	var buf = make([]byte, p.Options.PackageMaxLength)
+	var tmp = make([]byte, 1024)
+	for {
+		n, err := p.Conn.Read(tmp)
+		fmt.Println("read: ", string(tmp), ",err:", err)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		buf = append(buf, tmp[:n]...)
+	}
+	//todo
+	fmt.Println("buf1:", string(buf))
+	eofLen := len([]byte(p.Options.PackageEof))
+	bufLen := len(buf)
+	buf = buf[:bufLen-eofLen]
 	//todo
 	fmt.Println("buf2:", string(buf), len(string(buf)))
 	err = common.GetResult(buf, result)
